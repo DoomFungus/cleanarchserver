75c789231f2ad6e66798aaa7c351748e419235de
DoomFungus <unspeakableferretofdoom@gmail.com>
2019-05-26 22:54:24 +0300

    Initial commit

---
+++ src/main/java/kpi/cleanarch/cleanarchserver/CleanarchserverApplication.java
@@ -0,0 +1,13 @@
+package kpi.cleanarch.cleanarchserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanarchserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanarchserverApplication.class, args);
+    }
+
+}
